20b5ffc4749a495397607479323678eb41940511
Martin Halas <echoes.time@gmail.com>
2021-02-17 01:52:18 +0000

    basic for AND while loop example

--- packing-list.js
+++ packing-list.js
@@ -6,3 +6,11 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 console.log(packingList[3]);
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
